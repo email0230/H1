ab0b1f2d158efba25bbefa3a9091350d5b95ee21
email0230 <email0230@gmail.com>
2023-11-25 16:28:45 +0100

    saving progress

---
+++ h1_client/h1/replaceme/IRepositoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h1.replaceme
+{
+    public interface IRepositoryService<T> where T : class
+    {
+        void Create(T entity);
+        void Read(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        //they used to be returning a "serviceresult" object, check out why that is
+    }
+}

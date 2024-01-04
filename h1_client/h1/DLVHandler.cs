e17e5a655c71e165a46a15522d750209239333e8
email0230 <email0230@gmail.com>
2024-01-04 19:58:00 +0100

    initial dlv implementation
    lots of things missing!

---
+++ h1_client/h1/DLVHandler.cs
@@ -0,0 +1,45 @@
+﻿using h1.Models;
+using MongoDB.Bson.IO;
+using SharpCompress.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h1
+{
+    public static class DLVHandler
+    {
+        private static string ParseGroupsToString(ObservableCollection<Group> inputGroupCollection, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true, Encoding.UTF8))
+            {
+                foreach (Group group in inputGroupCollection)
+                {
+                    // Append a line for each group
+                    string line = $"placeholder"; // Customize this line as needed
+                    writer.WriteLine(line);
+                }
+            }
+            return null;
+        }
+        private static string ParseRoomsToString()
+        {
+            Hotel hotel = Hotel.GetInstance();
+            List<Room> rooms = hotel.Rooms;
+
+            return "haha";
+        }
+        public static string GenerateDLVFile(ObservableCollection<Group> input)
+        {
+            const string filepath = "dlvStuff/target/groups.txt";
+
+            string groupsString = ParseGroupsToString(input, filepath);
+            string roomsString = ParseRoomsToString();
+            return "haha";
+        }
+    }
+}
--- h1_client/h1/Views/MainWindow.xaml.cs
+++ h1_client/h1/Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using h1.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,6 +55,42 @@ namespace h1
         {
             RoomListWindow listWindow = new RoomListWindow();
             listWindow.Show();
+        }
+
+        private void debugDlvButton_Click(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<Group> groups = DebugInitGroupCollection();
+
+            DLVHandler.GenerateDLVFile(groups);
+        }
+
+        private static ObservableCollection<Group> DebugInitGroupCollection()
+        {
+            ObservableCollection<Group> groups = new ObservableCollection<Group>();
+
+            // Declare and add statically 2-3 groups, each with 2 guests
+            ObservableCollection<Guest> guests1 = new ObservableCollection<Guest>
+            {
+                new Guest{LastName = "Nowakowski", FirstName = "Piotr" },
+                new Guest{LastName = "adasd", FirstName = "ssss" }
+            };
+            Group group1 = new Group(guests1, "aa");
+            groups.Add(group1);
+
+            ObservableCollection<Guest> guests2 = new ObservableCollection<Guest>
+            {
+                new Guest{LastName = "sddd", FirstName = "aaaa" },
+                new Guest{LastName = "bbb", FirstName = "pitor" }
+            };
+            Group group2 = new Group(guests2, "grr");
+            groups.Add(group2);
+
+            group1.WantGroupToStayTogether = true;
+            group1.WantNoiseReduction = true;
+            group2.WantModularFurniture = true;
+            group2.WantSecurityFeatures = true;
+
+            return groups;
         }
     }
 }
